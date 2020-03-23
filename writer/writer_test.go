5ecb0acceee96560b1a7bf10e98626163727f006
Ben Pate <ben@pate.org>
2020-03-22 20:17:30 -0600

    Updating test cases
    Just getting it to complie right now.  Lots more to come.

--- writer/writer_test.go
+++ writer/writer_test.go
@@ -1,9 +1,25 @@
 package writer
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/davecgh/go-spew/spew"
+	"github.com/stretchr/testify/assert"
+)
 
 func TestWriter(t *testing.T) {
 
-	object = Read()
+	object := Accept(
+		Person("John Connor", "en").ID("john@connor.com"),
+		Document().URL("https://example.com/document/id"),
 	)
+
+	// Display the generated structure...
+	t.Log(spew.Sdump(object))
+
+	assert.Equal(t, "Accept", object["type"])
+
+	actor := object["actor"].(Object)
+	assert.Equal(t, "john@connor.com", actor["id"])
+	assert.Equal(t, "John Connor", actor["name"])
 }
